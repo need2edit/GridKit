10aa622bd5b6b6aea54b561ca6e2d198a95a5776
Jake Young <need2edit@gmail.com>
2017-02-11 20:26:33 -0500

    Initial Commit

---
+++ GridKit/GridKit.h
@@ -0,0 +1,19 @@
+//
+//  GridKit.h
+//  GridKit
+//
+//  Created by Jake Young on 2/11/17.
+//  Copyright © 2017 Jake Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GridKit.
+FOUNDATION_EXPORT double GridKitVersionNumber;
+
+//! Project version string for GridKit.
+FOUNDATION_EXPORT const unsigned char GridKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GridKit/PublicHeader.h>
+
+
